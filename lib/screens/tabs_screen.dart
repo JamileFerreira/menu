3791ef25e7ba542a5c38daafb3c9eb3b155972dd
jamile ferreira <jamileferreira1313@gmail.com>
2022-08-02 06:25:22 -0300

    Creating tabs and favorites screen

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import '../screens/categories_screen.dart';
+import 'favorite_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIdex = 0;
+  final List<Map<String, Object>> _screens = [
+    {'title': 'Lista de Categorias?', 'screen': CategoriesScreen()},
+    {'title': 'Meus Favoritos', 'screen': FavoriteScreen()},
+  ];
+
+  _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIdex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          _screens[_selectedScreenIdex]['title'] as String,
+        ),
+      ),
+      body: _screens[_selectedScreenIdex]['screen'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectScreen,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).colorScheme.secondary,
+        currentIndex: _selectedScreenIdex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categorias',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favorito',
+          ),
+        ],
+      ),
+    );
+  }
+}
